951de7cc561b78512202a876f4c907817d72b755
Marwan Al-Obaidi <marwan1alobaidi@live.fi>
2024-12-19 11:40:12 +0200

    Movie ID endpoint implemented

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
     res.send(movies);
 });
 
+app.get('/id/:id', (req, res) => {
+    var id = req.params.id;
+    var moviesById = movies.find(moviesById => moviesById.id == id);
+    if (moviesById === undefined) {
+        res.status(404).send('Movie Id ' + id + ' not found.');
+        console.log('Movie Id ' + id + ' not found.');
+    } else {
+        res.send(moviesById);
+        console.log(moviesById);
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
 });
